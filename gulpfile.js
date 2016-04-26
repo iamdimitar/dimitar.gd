57eace4b9b0c3111fcbb459543d7af7d14667a4d
Dimitar Dimitrov <dgdwazzaaa@gmai.com>
2016-04-26 14:35:20 -0400

    Added all the necessary files for the distribution

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 //gulp
 var gulp = require('gulp');
 //plugins
+var gutil = require('gulp-util');
 var vulcanize = require('gulp-vulcanize');
+var htmlmin = require('gulp-htmlmin');
 
 //variables
 var distributionFolder = 'distr';
 
-// Vulcanize
+// create a default task and just log a message
+gulp.task('message', function() {
+  return gutil.log('Gulp is running!');
+});
+
+
+// Vulcanize and minify
 gulp.task('vulcanize', function () {
 	return gulp.src('index.html')
 		.pipe(vulcanize({
@@ -17,9 +25,17 @@ gulp.task('vulcanize', function () {
   			inlineCss: true,
   			stripComments: true
 		}))
+		.pipe(htmlmin({collapseWhitespace: true}))
 		.pipe(gulp.dest(distributionFolder));
 });
 
+//Copy distribution files
+gulp.task('copyFiles', function() {
+  // copy any html files in source/ to public/
+  gulp.src('images/*.jpg').pipe(gulp.dest(distributionFolder + '/images'));
+  gulp.src('docs/*.pdf').pipe(gulp.dest(distributionFolder + '/docs'));
+  gulp.src('*.ico').pipe(gulp.dest(distributionFolder));
+});
 
 // Default Task
-gulp.task('default', ['vulcanize']);
+gulp.task('default', ['message', 'vulcanize', 'copyFiles']);
